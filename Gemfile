2daf371144f9f6a7f8f9d0fa131957aed47c09b9
Martinos <chabotm@gmail.com>
2011-12-29 17:43:23 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+gem 'guard'
+gem 'guard-test'
+gem 'logging'
+gem 'test-unit'
+gem 'rb-fsevent', RUBY_PLATFORM =~ /darwin/i ? {:require => false}: {:require => true}
+gem 'guard-test'
+gem 'growl'
---
+++ test/coco_test.rb
@@ -0,0 +1,12 @@
+require "logging"
+require "test/unit"
+
+$log = Logging::Logger["testlogger"]
+$log.add_appenders(Logging::Appenders.stdout)
+
+class AtExitConflictTest < Test::Unit::TestCase
+  def test_at_exit_conflict
+    $log.debug("test")
+    assert(true)
+  end
+end
